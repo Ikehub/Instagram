b8a594d6bf142519cdf691fb2ca43823402eb868
Ikenna Okonkwo <ikennaokonkwo2012@gmail.com>
2022-04-03 16:49:02 -0400

    Created feed and profile fragments

---
+++ app/src/main/java/com/example/instagram/fragments/FeedFragment.kt
@@ -0,0 +1,102 @@
+package com.example.instagram.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.instagram.MainActivity
+import com.example.instagram.Post
+import com.example.instagram.PostAdapter
+import com.example.instagram.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+
+open class FeedFragment : Fragment() {
+
+    lateinit var postsRecyclerView: RecyclerView
+    lateinit var adapter: PostAdapter
+    lateinit var swipeContainer: SwipeRefreshLayout
+    val allPosts = ArrayList<Post>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        swipeContainer = view.findViewById(R.id.swipeContainer)
+
+        swipeContainer.setOnRefreshListener {
+            queryPosts()
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(
+            android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light
+        )
+
+        postsRecyclerView = view.findViewById(R.id.postRecyclerView)
+
+        // Steps to populate RecyclerView
+        // 1. Create layout for each row in list
+        // 2. Create data source for each row (this is the Post class)
+        // 3. Create adapter that will bridge data and row layout
+        // 4. Set adapter on RecyclerView
+        adapter = PostAdapter(requireContext(), allPosts)
+        postsRecyclerView.adapter = adapter
+
+        // 5. Set layout manager on RecyclerView
+        postsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        queryPosts()
+    }
+
+    open fun queryPosts() {
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.addDescendingOrder("createdAt")
+        query.limit = 20
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                adapter.clear()
+                if (e != null) {
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(
+                                TAG, "Post: " + post.getDescription() + " , username: " +
+                                    post.getUser()?.username)
+                        }
+
+                        allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                        swipeContainer.isRefreshing = false
+                    }
+                }
+            }
+
+        })
+    }
+
+
+
+    companion object {
+        const val TAG = "FeedFragment"
+    }
+
+}
